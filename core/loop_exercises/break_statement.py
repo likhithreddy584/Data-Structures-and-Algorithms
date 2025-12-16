4b3deafe04ee32f0fb5c04950461ba400076cd82
Likhith Reddy <likhithreddy584@gmail.com>
2025-12-16 21:41:46 +0530

    Add functions to find a number greater than 20 and to skip printing the number 5

---
+++ core/loop_exercises/break_statement.py
@@ -0,0 +1,11 @@
+def find_number_greater_than_20():
+    numbers = [3, 8, 15, 20, 25, 30]
+
+    for num in numbers:
+        if num > 20:
+            print("Found it!")
+            break
+
+
+# function call
+find_number_greater_than_20()
---
+++ core/loop_exercises/print_skipnumber.py
@@ -0,0 +1,7 @@
+def skip_number_5():
+    for i in range(1,21):
+        if i ==5:
+            continue
+        print(i)
+
+skip_number_5()
