2625d82787602b79067e7c5a37bffb3cac2b5bc6
Likhith Reddy <likhithreddy584@gmail.com>
2025-12-16 11:02:31 +0530

    Add natural numbers generator with user input and sequential outputs

---
+++ core/loop_exercises/natural_numbers.py
@@ -0,0 +1,16 @@
+
+def natural_numbers(n):
+    for i in range(1, n + 1):
+        print(i)
+
+n = int(input("enter a  number :"))
+
+natural_numbers(n)
+
+print("n+1 natural numbers starting...")
+
+natural_numbers(n+1)
+
+print("n+2 natural numbers starting...")
+
+natural_numbers(n+2)
