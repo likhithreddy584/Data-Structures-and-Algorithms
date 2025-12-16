739d425d85e53330d925cc5bf9dc2190fda80653
Likhith Reddy <likhithreddy584@gmail.com>
2025-12-16 11:39:19 +0530

    Rename variable for consistency in print_items function

--- core/loop_exercises/print_list.py
+++ core/loop_exercises/print_list.py
@@ -2,6 +2,6 @@ def print_items(items):
     for item in items:
         print(item)
 
-my_list = ["Apple", "Banana", "Mango", "Orange"]
+items = ["Apple", "Banana", "Mango", "Orange"]
 
-print_items(my_list)
+print_items(items)
